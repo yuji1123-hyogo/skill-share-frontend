669d5e42207d64c3bb7871f16df912c30fa14853
Yuji Yamaguchi <yuji1123200113@gmail.com>
2024-12-09 15:02:13 +0900

    modyfy for deploy

--- src/model/httpApiCrients/apiCrientSetting.js
+++ src/model/httpApiCrients/apiCrientSetting.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const apiClient = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL,  
+    baseURL: "https://skill-share-backend.onrender.com",  
     headers: {
       'Content-Type': 'application/json', //request.bodyがjson形式であることの明示
     },
